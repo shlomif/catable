b78dec5decf27e3c2912c59a243a0a6679325ac7
Shlomi Fish <shlomif@iglu.org.il>
2009-06-27 19:04:26 +0300

    Added the add posts controller and view.

--- App-Catable/lib/App/Catable/Controller/Posts.pm
+++ App-Catable/lib/App/Catable/Controller/Posts.pm
@@ -6,7 +6,7 @@ use base 'Catalyst::Controller';
 
 =head1 NAME
 
-App::Catable::Controller::Posts - Catalyst Controller
+App::Catable::Controller::Posts - Catalyst Posts Controller
 
 =head1 DESCRIPTION
 
@@ -14,8 +14,29 @@ Catalyst Controller.
 
 =head1 METHODS
 
+=head2 add
+
+Add a post.
+
 =cut
 
+sub add : Local {
+    # Retrieve the usual Perl OO '$self' for this object. $c is the Catalyst
+    # 'Context' that's used to 'glue together' the various components
+    # that make up the application
+    my ($self, $c) = @_;
+
+    # Retrieve all of the book records as book model objects and store in the
+    # stash where they can be accessed by the TT template
+    # $c->stash->{books} = [$c->model('DB::Book')->all];
+    # But, for now, use this code until we create the model later
+    $c->stash->{books} = '';
+
+    # Set the TT template to use.  You will almost always want to do this
+    # in your action methods (action methods respond to user input in
+    # your controllers).
+    $c->stash->{template} = 'posts/add.tt2';
+}
 
 =head2 index 
 
---
+++ App-Catable/lib/App/Catable/View/TT.pm
@@ -0,0 +1,41 @@
+package App::Catable::View::TT;
+
+use strict;
+use base 'Catalyst::View::TT';
+
+use App::Catable;
+
+__PACKAGE__->config(
+    # Change default TT extension
+    TEMPLATE_EXTENSION => '.tt2',
+    # Set the location for TT files
+    INCLUDE_PATH => [
+    App::Catable->path_to( 'root', 'src' ),
+    ],
+);
+
+=head1 NAME
+
+App::Catable::View::TT - TT View for App::Catable
+
+=head1 DESCRIPTION
+
+TT View for App::Catable. 
+
+=head1 SEE ALSO
+
+L<App::Catable>
+
+=head1 AUTHOR
+
+Shlomi Fish, L<http://www.shlomifish.org/>
+
+=head1 LICENSE
+
+This library is distributed under the MIT/X11 License: 
+
+L<http://www.opensource.org/licenses/mit-license.php>
+
+=cut
+
+1;
