ef4d0aa4ad35182aa75c328ac7ff1d491768aa0f
Alastair Douglas <alastair.douglas+github@gmail.com>
2009-07-31 21:16:43 +0100

    Change test data to pass in plaintext password.

--- App-Catable/t/lib/AppCatableTestSchema.pm
+++ App-Catable/t/lib/AppCatableTestSchema.pm
@@ -145,7 +145,7 @@ sub create_test_data {
 
     $schema->resultset('Account')->create( {
         username    => 'user',
-        password    => '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8', # 'password' SHA-1
+        password    => 'password', 
     } );
 
     return;
