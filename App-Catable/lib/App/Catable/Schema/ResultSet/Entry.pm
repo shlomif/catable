89b8b23bb5b102ffd73e998a0e751e5942606cd7
Alastair Douglas <alastair.douglas+github@gmail.com>
2009-09-13 01:23:24 +0100

    More Entry ResultSet helper methods
    This subclassing ResultSet is a bloody marvellous idea.

---
+++ App-Catable/lib/App/Catable/Schema/ResultSet/Entry.pm
@@ -0,0 +1,110 @@
+package App::Catable::ResultSet::Entry;
+
+use strict;
+use warnings;
+use base qw( DBIx::Class::ResultSet );
+
+=head1 NAME
+
+App::Catable::ResultSet::Entry - ResultSet class representing Entries.
+
+=head1 SYNOPSIS
+
+    my $posts = App::Catable->model('BlogDB')
+                            ->resultset('Entry)
+                            ->posts;
+
+    while ( my $post = $posts->next ) {
+        # do something with this post.
+    }
+
+=head1 DESCRIPTION
+
+A convenience class. ResultSet is designed to be extended, so here's
+an extension that deals with the Entry table
+
+=head1 METHODS
+
+=head2 posts
+
+    $rs->posts();
+
+Returns all rows in the result set that are posts, i.e. have no parent.
+
+=cut
+
+sub posts {
+    my $self = shift;
+    return $self->search( [
+        { parent => undef },
+        { parent => 0 }
+    );
+}
+
+=head2 by_blogs
+
+    $rs->by_blogs( $blog_id_or_object, ... );
+
+Returns a resultset filtered by the blog(s) you specify.
+
+=cut
+
+sub by_blogs {
+    my $self = shift;
+    my @blogs = @_;
+
+    return $self->search( {
+        blog_id => {
+            '-in' => [ map {_blog_id( $_ ) } @blogs ],
+        },
+    } );
+}
+
+=head2 published_posts
+
+Select only those Entries that are published. This flag only applies
+to top-level entries - posts - and not comments, hence the name of the
+method. Published also requires that the pubdate is earlier than now.
+
+=cut
+
+sub published_posts {
+    my $self = shift;
+
+    return $self->posts->search( {
+        can_be_published => 1,
+        pubdate => { "<=" => \"DATETIME('NOW')" },
+    });
+}
+
+## Makes sure the provided thing is an id, not an object.
+sub _blog_id {
+    my $blog = shift;
+
+    return ref $blog ? $blog->id : $blog;
+}
+
+=head1 SEE ALSO
+
+L<App::Catable::Schema>, L<App::Catable>, L<DBIx::Class>
+
+=head1 AUTHOR
+
+Shlomi Fish L<http://www.shlomifish.org/>
+
+Alastair Douglas L<http://www.podcats.in/>
+
+=head1 LICENSE
+
+This module is free software, available under the MIT X11 Licence:
+
+L<http://www.opensource.org/licenses/mit-license.php>
+
+Copyright by Shlomi Fish, Alastair Douglas 2009.
+
+=cut
+
+1;
+
+__END__
+
