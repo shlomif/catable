ba2c99f621bf31e07148a087b6fef2b5d1127749
Shlomi Fish <shlomif@iglu.org.il>
2010-07-08 23:47:39 +0300

    Updated to latest catalyst plugins.
    Also added an item to the TODO for not showing login.

--- App-Catable/script/app_catable_test.pl
+++ App-Catable/script/app_catable_test.pl
@@ -1,20 +1,7 @@
 #!/usr/bin/env perl
 
-use strict;
-use warnings;
-use Getopt::Long;
-use Pod::Usage;
-use FindBin;
-use lib "$FindBin::Bin/../lib";
-use Catalyst::Test 'App::Catable';
-
-my $help = 0;
-
-GetOptions( 'help|?' => \$help );
-
-pod2usage(1) if ( $help || !$ARGV[0] );
-
-print request($ARGV[0])->content . "\n";
+use Catalyst::ScriptRunner;
+Catalyst::ScriptRunner->run('App::Catable', 'Test');
 
 1;
 
@@ -27,7 +14,7 @@ app_catable_test.pl - Catalyst Test
 app_catable_test.pl [options] uri
 
  Options:
-   -help    display this help and exits
+   --help    display this help and exits
 
  Examples:
    app_catable_test.pl http://localhost/some_action
