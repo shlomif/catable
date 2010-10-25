278598eb9e115a9ecf1a25bbaa6ec030c871e74d
Shlomi Fish <shlomif@iglu.org.il>
2010-10-25 17:32:19 +0200

    Add and convert to next_in_rs.
    I added a function to the TT view called next_in_rs() that handles
    returning the next result from a result-set gracefully and so got rid of
    the TRY ... CATCH in the TT views.

--- App-Catable/lib/App/Catable/View/TT.pm
+++ App-Catable/lib/App/Catable/View/TT.pm
@@ -3,9 +3,18 @@ package App::Catable::View::TT;
 use strict;
 use warnings;
 
+use Catalyst::View::TT 0.35;
+
 use base 'Catalyst::View::TT';
 
 use App::Catable;
+
+sub next_in_rs {
+    my ($self, $c, $rs) = @_;
+
+    # Returns "" instead of undef or the next result.
+    return $rs->next() || "";
+}
 
 __PACKAGE__->config(
     # Change default TT extension
@@ -16,7 +25,9 @@ __PACKAGE__->config(
     ],
     WRAPPER => "wrapper.tt2",
     STRICT => 1,
+    expose_methods => [qw/next_in_rs/],
 );
+
 
 =head1 NAME
 
@@ -25,6 +36,23 @@ App::Catable::View::TT - TT View for App::Catable
 =head1 DESCRIPTION
 
 TT View for App::Catable. 
+
+=head1 METHODS
+
+=head2 next_in_rs($self, $c, $rs)
+
+An exposed method that is useful to fetch the next result of a 
+L<DBIx::Class> resultset. It returns
+an empty string (which is still false) instead of undef() as the next result
+or the next result if it's a valid result.
+
+Use in a template like so:
+
+    [% WHILE post = next_in_rs(posts_rs) %]
+
+    [%# Do something with 'post' %]
+
+    [% END %]
 
 =head1 SEE ALSO
 
